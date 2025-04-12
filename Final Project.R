16e0685e4675ae4bbe2efed1a111e9055d6e76fa
songgiaan <songgiaan.nguyen@gmail.com>
2025-04-12 03:05:05 -0400

    Final Project.R

--- Final Project.R
+++ Final Project.R
@@ -1,8 +1,12 @@
 ## Load library and packages ## ----------
+install.packages('caret')
+library(caret)
+
 library(tidyverse)
 library(ggplot2)
 library(readr)
 library(dplyr)
+
 
 ## Set working directory & Load dataset ## ----------
 lungcancer <- read_csv('lung_cancer_mortality_data_test_v2_small.csv')
@@ -11,126 +15,71 @@ lungcancer <- read_csv('lung_cancer_mortality_data_test_v2_small.csv')
 glimpse(lungcancer)
 
 
-## -------- Data Cleaning / EDA -------- ##
+## -------- Data Cleaning & Preprocessing -------- ##
 
 ## Check for missing data
 sum(is.na(lungcancer)) # there is no missing data
-
-# Check what values are in this variable
-#unique(lungcancer$family_history)
-
-## Convert into binary data -------
-#lungcancer$family_history = ifelse(lungcancer$family_history == 'Yes', 1, 0)
 
 
 ## Convert chr data into factors -------
 lungcancer <- lungcancer |>
   mutate(across(where(is.character), as.factor))
 
-str(lungcancer) # check if it works
-
 # Convert binary data into factors
 lungcancer <- lungcancer |>
   mutate(across(c("hypertension", "asthma", "cirrhosis", "other_cancer", "survived")))
 
-## -------- EDA -------- ##
-table(lungcancer$survived)
+str(lungcancer) # check if it works
 
-# Distribution plots
+
+
+## -------- EDA -------- ##
+
+# General distribution plots on all numeric variables
 lungcancer |>
   select(where(is.numeric)) |>
   pivot_longer(cols = -c(survived, id)) |>
   ggplot(aes(x = value)) +
   geom_histogram(bins = 30, fill = "skyblue", color = "white") +
   facet_wrap(~name, scales = "free")
- 
+
+# Age range
+range(lungcancer$age)
+# Breaking age groups
+lungcancer$age_group <- cut(lungcancer$age,
+                    breaks = c(seq(0, 90, by = 15), 100),
+                    labels = c("0-15", "16-30", "31-45", "46-60", "61-75", "76-90", "91-100"),
+                    right = FALSE)
+
+# Distribution of Age Groups visualization
+ggplot(lungcancer, aes(x = age_group)) +
+  geom_bar(fill = "lightgreen", color = "black") +
+  labs(title = "Age Groups Distribution",
+       x = "Age Group",
+       y = "Count") +
+  theme_minimal()
+
+
+# Distribution of Survival rates
+ggplot(lungcancer, aes(x = survived)) + 
+  geom_bar(fill = "lightgreen") + labs(title = "Survival Distribution")
+
+# Distribution of Age
+ggplot(lungcancer, aes(x = age)) + 
+  geom_histogram(binwidth = 5, fill = "lightgreen", color='black') + labs(title = "Age Distribution")
+
+
+# Survival Proportion by Treatment type
 ggplot(lungcancer, aes(x = treatment_type, fill = factor(survived))) +
   geom_bar(position = "fill") +
-  labs(y = "Proportion", fill = "Survived")
-
-lungcancer %>%
-  count(treatment_type, survived) %>%
-  ggplot(aes(x = treatment_type, y = n, fill = factor(survived))) +
-  geom_col(position = "fill") +
-  labs(title = "Survival Rate by Treatment Type", 
-       x = "Treatment Type", 
+  labs(title = "Survival Proportion by Treatment Type",
+       x = "Treatment Type",
        y = "Proportion", fill = "Survived")
 
-
-ggplot(lungcancer, aes(x = smoking_status, fill = factor(survived))) +
-  geom_bar(position = "fill") +
-  labs(y = "Proportion", fill = "Survived")
-
-## -------- Modeling -------- ##
-
-## Research question 1: -------
-# Can we predict whether a patient will survive based on pre-treatment 
-#health profile and cancer stage?
-Q1 <- glm(survived ~ age + gender + cancer_stage + family_history
-          + smoking_status + bmi + cholesterol_level + hypertension
-          + asthma + cirrhosis + other_cancer,
-          data=lungcancer, family='binomial')
-summary(Q1)
-exp(coef(Q1))     # Find odds ratio
-
-# Predict response variable
-Q1_predicted <- predict(Q1, type = "response")
-predicted_class <- ifelse(Q1_predicted > 0.5, 1, 0)
-
-# check prediction accuracy
-mean(predicted_class == lungcancer$survived)
-
-table(Predicted = predicted_class, Actual = lungcancer$survived)
-
-## Research question 2: -------
-#
-Q2 <- glm(survived ~ age + gender + cancer_stage + family_history
-          + smoking_status + bmi + cholesterol_level + hypertension
-          + asthma + cirrhosis + other_cancer + treatment_type,
-          data=lungcancer, family='binomial')
-summary(Q2)
-
-# Predict response variable
-Q2_predicted <- predict(Q2, type = "response")
-predicted_class_2 <- ifelse(Q2_predicted > 0.5, 1, 0)
-
-# check prediction accuracy
-mean(predicted_class_2 == lungcancer$survived)
-
-table(Predicted = predicted_class_2, Actual = lungcancer$survived)
-
-# Add in necessary variable:
-
-# Assumption check:
-
-
-lungcancer %>%
-  count(hypertension, survived) %>%
-  ggplot(aes(x = factor(hypertension), y = n, fill = factor(survived))) +
-  geom_col(position = "fill") +
-  labs(title = "Survival Rate by Hypertension Status", 
-       x = "Hypertension (0 = No, 1 = Yes)", 
-       y = "Proportion", fill = "Survived")
-
-
-# show survival rate
-lungcancer %>%
-  group_by(treatment_type) %>%
-  summarise(
-    total = n(),
-    survival_rate = mean(survived)
-  ) %>%
-  ggplot(aes(x = treatment_type, y = survival_rate)) +
-  geom_col(fill = "steelblue") +
-  geom_text(aes(label = scales::percent(survival_rate)), vjust = -0.5) +
-  labs(title = "Survival Rate by Treatment Type", 
-       x = "Treatment Type", y = "Survival Rate") +
-  scale_y_continuous(labels = scales::percent_format())
-
-# OR 
-lungcancer %>%
-  group_by(treatment_type) %>%
-  summarise(survival_rate = mean(survived)) %>%
+# Survival Rate by Treatment type
+lungcancer |>
+  group_by(treatment_type) |>
+  summarise(survival_rate = mean(survived)) |>
   ggplot(aes(x = treatment_type, y = survival_rate)) +
   geom_col(fill = "skyblue") +
   geom_text(aes(label = scales::percent(survival_rate, accuracy = 0.1)), 
@@ -138,17 +87,96 @@ lungcancer %>%
   labs(title = "Survival Rate by Treatment Type", y = "Survival Rate", x = "Treatment") +
   scale_y_continuous(labels = scales::percent_format())
 
-## Assumption check:
-# Linearity check
-lungcancer_test <- lungcancer %>% mutate(log_age = log(age + 1))
-model_test <- glm(survived ~ age + log_age + ..., data = lungcancer_test, family = "binomial")
-summary(model_test)
+
+# Survival Proportion by Age Groups
+ggplot(lungcancer, aes(x = age_group, fill = factor(survived))) +
+  geom_bar(position = "fill") +
+  labs(title = "Survival Proportion by Age Group",
+       x = "Age Group",
+       y = "Proportion", fill = "Survived")
+
+# Survival Rate by Age Groups
+lungcancer |>
+  group_by(age_group) |>
+  summarise(survival_rate = mean(survived)) |>
+  ggplot(aes(x = age_group, y = survival_rate)) +
+  geom_col(fill = "skyblue") +
+  geom_text(aes(label = scales::percent(survival_rate, accuracy = 0.1)), 
+            vjust = -0.5) +
+  labs(title = "Survival Rate by Age Groups", y = "Survival Rate", x = "Age Group") +
+  scale_y_continuous(labels = scales::percent_format())
 
 
-# Multicollinearity
-library(car)
-vif(Q1)
+# Survival Proportion by Smoking Status
+ggplot(lungcancer, aes(x = smoking_status, fill = factor(survived))) +
+  geom_bar(position = "fill") +
+  labs(title = "Survival Proportion by Smoking Status",
+       x = "Smoking Status",
+       y = "Proportion", fill = "Survived")
 
-plot(Q1, which = 4)  # Cook’s Distance
+# Survival Rate by Smoking Status
+lungcancer |>
+  group_by(smoking_status) |>
+  summarise(survival_rate = mean(survived)) |>
+  ggplot(aes(x = smoking_status, y = survival_rate)) +
+  geom_col(fill = "skyblue") +
+  geom_text(aes(label = scales::percent(survival_rate, accuracy = 0.1)), 
+            vjust = -0.5) +
+  labs(title = "Survival Rate by Smoking Status", y = "Survival Rate", x = "Smoking Status") +
+  scale_y_continuous(labels = scales::percent_format())
 
 
+# Survival Rate by Cancer Stage
+lungcancer |>
+  group_by(cancer_stage) |>
+  summarise(survival_rate = mean(survived)) |>
+  ggplot(aes(x = cancer_stage, y = survival_rate)) +
+  geom_col(fill = "skyblue") +
+  geom_text(aes(label = scales::percent(survival_rate, accuracy = 0.1)), 
+            vjust = -0.5) +
+  labs(title = "Survival Rate by Cancer Stage", y = "Survival Rate", x = "Cancer Stage") +
+  scale_y_continuous(labels = scales::percent_format())
+
+
+# Normality check for continuous variable (age) - QQ plot
+qqnorm(lungcancer$age)
+qqline(lungcancer$age, col = "red")
+
+
+## -------- Assumption Check -------- ##
+
+
+
+## -------- Modeling -------- ##
+
+## Research question 1: -------
+# Which pre-treatment factors (age, smoking status, cancer stage, etc.) 
+# are significantly associated with higher survival rates?
+
+Q1 <- glm(survived ~ age + gender + cancer_stage + family_history
+          + smoking_status + bmi + cholesterol_level + hypertension
+          + asthma + cirrhosis + other_cancer,
+          data=lungcancer, family='binomial')
+summary(Q1)
+
+
+
+## Research question 2: -------
+# Which treatment types (surgery, chemotherapy, radiation, or combined) 
+# are associated with higher mortality rates?
+Q2 <- glm(survived ~ treatment_type, 
+          data=lungcancer, family = 'binomial')
+
+summary(Q2)
+
+
+## Research question 3: -------
+# Can we predict the likelihood of survival based on 
+# a combination of pre-treatment factors and treatment types?
+Q3 <- glm(survived ~ age + gender + cancer_stage + family_history
+          + smoking_status + bmi + cholesterol_level + hypertension
+          + asthma + cirrhosis + other_cancer,
+          data=lungcancer, family = 'binomial')
+
+# View the model summary
+summary(Q3)
