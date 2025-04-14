e441ff34eb7d6171388428c84f0c703644159a89
Thuy Dong <123421460+ThuyDong0215@users.noreply.github.com>
2025-04-14 02:00:43 -0400

    Update Analytics_Day_code.R

--- Analytics_Day_code.R
+++ Analytics_Day_code.R
@@ -9,7 +9,7 @@ library(dplyr)
 
 
 ## ---------- Set working directory & Load dataset ---------- ##
-lungcancer <- read_csv('lung_cancer_mortality_data_test_v2_small.csv')
+lungcancer <- read_csv('lung_cancer_mortality_data_test_v2.csv')
 # read_csv is faster than read.csv at reading large dataset
 
 glimpse(lungcancer)
@@ -109,6 +109,20 @@ lungcancer |>
   labs(title = "Survival Rate by Cancer Stage", y = "Survival Rate", x = "Cancer Stage") +
   scale_y_continuous(labels = scales::percent_format())
 
+# Survival Rate by Treatment type and cancer stage
+lungcancer |>
+  group_by(treatment_type, cancer_stage) |>
+  summarise(survival_rate = mean(survived)) |>
+  ggplot(aes(x = cancer_stage, y = survival_rate, fill = treatment_type)) +
+  geom_col(position = "dodge") + # To make sure the bars are displayed side by side
+  geom_text(aes(label = scales::percent(survival_rate, accuracy = 0.1)),
+            position = position_dodge(width = 0.9), # tells ggplot to place bars side by side, control how much space is between them. 
+            vjust = -0.5) + # Set the space between the text and the bar
+  labs(title = "Survival Rate by Treatment Type and cancer Stage", 
+       y = "Survival Rate", 
+       x = "Treatment and Stage") +
+  scale_y_continuous(labels = scales::percent_format()) +
+  scale_fill_manual(values = c("lightblue", "lightgreen", "lightcoral", "lightyellow"))
 
 
 ## -------- Modeling -------- ##
@@ -136,8 +150,48 @@ library(car)
 vif(Model1)
 vif(Model2)
 
+##----------------------------Pie Chart------------------------------##
+# Set up 2 rows and 3 columns layout for the pie charts
 
+par(mfrow = c(2,2),
+    mar = c(1,1,1,1),
+    oma = c(0,0,0,0))
 
+# List all the variables to plot in the pie chart 
 
+vars <- list(
+  "Treatment Types" = lungcancer$treatment_type,
+  "Cancer Stage" = lungcancer$cancer_stage,
+  "Smoking Status" = lungcancer$smoking_status,
+  "Survived" = lungcancer$survived
+)
+
+# Define colors for each variable
+colors_list <- list(
+  "Treatment Types" = c("lightblue", "lightgreen", "lightcoral", "lightyellow" ),
+  "Cancer Stage" = c("Red", "orange", "yellow", "green"),
+  "Smoking Status" = c("purple", "pink", "blue", "cyan"),
+  "Survived" = c("lightgreen", "lightpink")
+  
+)
+
+# Plot the pie chart
+#Set the variables that needed for plotting
+for (title in names(vars)) {
+  variable_counts <- table(vars[title]) # Get the variabel name 
+  percentage <- round(100 * variable_counts / sum(variable_counts), 1) # Calculate the percentage for each variable
+  labels <- paste(names(variable_counts), ": ", percentage, "%")  # Create the labels
+  colors <- colors_list[[title]] # Set the color for the pie chart
+  
+  # Plot the pie chart with title and legend
+  pie(variable_counts,
+      labels = NA,
+      col= colors,
+      radius = 0.2)
+  
+  title(main = title, line = -10, cex.main = 1.6) # Set the features for the text 
+  legend("bottom", legend = labels, fill=colors, bty="n", inset=0.2, cex = 1.2) # Set the features for the legends 
+  
+}
 
 
